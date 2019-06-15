238df66955caea03b8e2692d418cf142efadeef5
Nicholas Grogg <nicholas_grogg6505@hotmail.com>
2019-06-15 00:22:52 +0000

    Upload New File

---
+++ source/Obstacles.hx
@@ -0,0 +1,77 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.animation.FlxAnimation;
+import flixel.math.FlxRandom;
+import flixel.util.FlxSpriteUtil;
+import flixel.math.FlxVelocity;
+import flixel.math.FlxPoint;
+
+
+class Obstacles extends FlxSprite
+{
+
+	public function new() 
+	{
+		super();
+	}
+
+	public function init(X:Int = 0, Y:Int = 0, VelocityX:Float = 0, VelocityY:Float = 0):Obstacles
+	{
+		var spriteNum = FlxG.random.int(1,4);
+		if(spriteNum == 1)
+		{
+			loadGraphic("assets/obstacle.png");
+		}
+		if(spriteNum == 2)
+		{
+			loadGraphic("assets/obstacle2.png");
+		}
+		if(spriteNum == 3)
+		{
+			loadGraphic("assets/obstacle3.png");
+		}
+		if(spriteNum == 4)
+		{
+			loadGraphic("assets/obstacle4.png");
+		}
+
+		// Position stuff, narrower margin for harder difficulty
+		if(MenuState.Difficulty == 3)
+		{
+			x = FlxG.width / 4 + FlxG.random.int(0,400);
+			y = FlxG.height / 2 + FlxG.random.int(-200,200);
+		}
+		else
+		{
+			x = FlxG.width / 2 + FlxG.random.int(0,100);
+			y = FlxG.height / 2 + FlxG.random.int(-200,200);
+		}
+		
+		// Velocity stuff
+
+		// If easy mode
+		if(MenuState.Difficulty == 1)
+		{ 
+			velocity.x = FlxG.random.int(-25,-75);
+			velocity.y = 0;
+		}
+		// If medium mode		
+		if(MenuState.Difficulty == 2)
+		{ 
+			velocity.x = FlxG.random.int(-50,-100);
+			velocity.y = 0;
+		}
+
+		// if hard mode, also activate collision!
+		if(MenuState.Difficulty == 3)
+		{ 
+			velocity.x = FlxG.random.int(-400,-650);
+			velocity.y = FlxG.random.int(-100, 100);
+		}
+
+		return this;
+	}	
+}
